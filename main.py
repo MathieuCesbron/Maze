877444d7a78b1b6a32ae349f2c49368ce146025a
MathieuCesbron <mathieu.cesbron@ecam-rennes.com>
2019-12-29 19:22:10 +0100

    new hp

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from stable_baselines import PPO2
 import numpy as np
 from env import MazeEnv
 
-world = np.array([[1, 0, 4, 2, 0],
-                  [4, 0, 0, 4, 0],
-                  [3, 3, 0, 0, 0],
-                  [0, 3, 3, 0, 0],
-                  [0, 0, 5, 0, 3]])
+world = np.array([[1, 0, 2, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [4, 0, 0, 0, 0],
+                  [0, 0, 3, 3, 0],
+                  [0, 0, 3, 5, 0]])
 
 env = DummyVecEnv([lambda: MazeEnv(world)])
 
 # Training
 print('TRAINING')
 model = PPO2(MlpPolicy, env, learning_rate=0.001, gamma=0.000001, lam=0)
-model.learn(100000)
+model.learn(1000000)
 
 # Testing
 print('TESTING')
